4ff24adc5f6846dbb52617002d141dedf2f4692a
Brian Hicks <brian@brianthicks.com>
2020-01-03 08:31:33 -0600

    nixify

---
+++ shell.nix
@@ -0,0 +1,20 @@
+{ ... }:
+let
+  sources = import ./nix/sources.nix;
+
+  nixpkgs = import sources.nixpkgs { };
+
+  niv = import sources.niv { };
+in with nixpkgs;
+stdenv.mkDerivation {
+  name = "elm-wave-function-collapse";
+  buildInputs = [
+    niv.niv
+    git
+
+    # Elm
+    elmPackages.elm
+    elmPackages.elm-format
+    elmPackages.elm-test
+  ];
+}
