6bc76a80d9574b4154bd59bc1f37dc20cea4f726
Brian Hicks <brian@brianthicks.com>
2020-01-07 15:35:23 -0500

    add entropies

--- src/Wave.elm
+++ src/Wave.elm
@@ -6,6 +6,7 @@ import AssocSet as Set exposing (Set)
 import Cell exposing (Cell)
 import Css
 import Grid exposing (Grid)
+import Heap exposing (Heap)
 import Html.Styled as Html exposing (Html)
 import Html.Styled.Attributes exposing (css)
 
@@ -14,6 +15,7 @@ type Wave a
     = Wave
         { items : Grid (Cell a)
         , probabilities : Dict a Int
+        , entropies : Heap { row : Int, column : Int, entropy : Float }
         }
 
 
@@ -44,18 +46,60 @@ init { width, height } windows =
                 )
                 Dict.empty
                 windows
+
+        startingEntropy =
+            entropy probabilities (Set.fromList windows)
     in
     case Grid.fromRowsAndColumnsArray rowsAndColumns of
         Ok grid ->
             Wave
                 { items = grid
                 , probabilities = probabilities
+                , entropies =
+                    List.range 0 height
+                        |> List.concatMap
+                            (\row ->
+                                List.map
+                                    (\col ->
+                                        { row = row
+                                        , column = col
+                                        , entropy = startingEntropy
+                                        }
+                                    )
+                                    (List.range 0 width)
+                            )
+                        |> Heap.fromList (Heap.smallest |> Heap.by .entropy)
                 }
 
         -- TODO: there should be a better way to deal with this if this
         -- becomes a library.
         Err problem ->
             Debug.todo (Debug.toString problem)
+
+
+entropy : Dict a Int -> Set a -> Float
+entropy probabilities possibilities =
+    let
+        total =
+            toFloat (List.sum (Dict.values probabilities))
+    in
+    possibilities
+        |> Set.toList
+        |> List.map
+            (\item ->
+                let
+                    frequency =
+                        probabilities
+                            |> Dict.get item
+                            |> Maybe.withDefault 0
+                            |> toFloat
+
+                    probability =
+                        frequency / total
+                in
+                -probability * logBase 2 probability
+            )
+        |> List.sum
 
 
 view : (Set a -> Html msg) -> Wave a -> Html msg
